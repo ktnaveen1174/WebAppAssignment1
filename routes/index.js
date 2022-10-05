6a8b42b14511a5f5edab722b5fc3ead0ba68517b
ktnaveen1174 <nkanmani@my.centennialcollege.ca>
2022-10-04 21:27:12 -0400

    4th commit

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('home', { title: 'Home'});
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('home', { title: 'Home'});
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('aboutme', { title: 'About'});
 });
 
 /* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+router.get('/product', function(req, res, next) {
+  res.render('product', { title: 'Products'});
 });
 
 /* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Services'});
 });
 
 /* GET Contact Us page. */
